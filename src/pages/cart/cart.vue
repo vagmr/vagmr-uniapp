540b3a3938d9bd5aac33590141503c677d5bcae7
vagmr <2737165206@qq.com>
2023-10-17 18:21:24 +0800

    feat:新建地址和修改地址的基本渲染

--- src/pages/cart/cart.vue
+++ src/pages/cart/cart.vue
@@ -3,9 +3,14 @@
 </script>
 
 <template>
-  <view class="cart">cart</view>
+  <view class="cart">还在开发中.............</view>
 </template>
 
 <style lang="scss">
 //
+.cart {
+  margin: 40px auto;
+  text-align: center;
+  font-size: 24px;
+}
 </style>
